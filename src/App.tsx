134597f5c9b33dee8d6f253a79f886ff4b82d3b0
Richard Michalek <micr02@vse.cz>
2024-05-29 10:07:54 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { Fragment, memo, useEffect, useState, useTransition } from 'react';
+import { text } from './text';
+
+const BookText = ({ highlight }: { highlight: string }) => {
+  if (highlight.length < 2) return <>{text}</>;
+
+  const content = text.split(highlight).map((textEntry, index, arr) => (
+    <Fragment key={index}>
+      {textEntry}
+      {index < arr.length - 1 && (
+        <span style={{ background: 'yellow' }}>{highlight}</span>
+      )}
+    </Fragment>
+  ));
+
+  return <>{content}</>;
+};
+
+const MemoedBookText = memo(BookText);
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        onChange={({ target }) => {
+          startTransition(() => {
+            setQuery(target.value);
+          });
+        }}
+        value={query}
+        type="text"
+        style={{ fontSize: '3rem' }}
+      />
+      <br />
+      {isPending ? <p>Loading...</p> : <MemoedBookText highlight={query} />}
+    </div>
+  );
+}
+
+export default App;
